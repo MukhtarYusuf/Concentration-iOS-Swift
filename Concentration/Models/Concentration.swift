e2bab54099770e51f6d8a822dce7f91082400135
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-07-15 19:31:26 +0100

    Added Concentration Struct
    Added Concentration struct to encapsulate logic for playing concentration game

---
+++ Concentration/Models/Concentration.swift
@@ -0,0 +1,13 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Mukhtar Yusuf on 7/1/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import Foundation
+
+struct Concentration {
+    private(set) var cards = [Card]()
+}

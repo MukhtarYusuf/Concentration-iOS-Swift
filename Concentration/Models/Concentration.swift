4afe212b1e6380507400be2b4c01b5c9ca4fbd79
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-07-17 17:47:30 +0100

    Implemented indexOfOneAndOnlyFaceUpCard
    Implemented get{} and set{} for indexOfOneAndOnlyFaceUpCard for game logic

--- Concentration/Models/Concentration.swift
+++ Concentration/Models/Concentration.swift
@@ -10,6 +10,28 @@ import Foundation
 
 struct Concentration {
     private(set) var cards = [Card]()
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+            
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
 }
 
 extension Collection {

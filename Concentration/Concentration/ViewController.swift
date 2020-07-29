bd953d3a526d94e308209ffcca0f406f26bddc28
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-07-29 20:07:11 +0100

    Added Extension to Int
    Added arc4Rrandom var to Int to get a random int between 0 and the value of the int value

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -60,3 +60,16 @@ class ViewController: UIViewController {
     }
     
 }
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
+}
+

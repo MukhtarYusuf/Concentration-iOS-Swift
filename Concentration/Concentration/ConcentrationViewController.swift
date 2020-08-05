19bb69090a15c98ce8e8657d7d1b4c405b87c05f
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-08-05 19:31:52 +0100

    Code Refactoring
    - Changed ViewController to ConcentrationViewController
    - Started Refactoring Code

--- Concentration/Concentration/ConcentrationViewController.swift
+++ Concentration/Concentration/ConcentrationViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConcentrationViewController: UIViewController {
     
     // MARK: Outlets
     @IBOutlet private weak var flipCountLabel: UILabel! {
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     }
     var numberOfPairsOfCards: Int {
         get {
-            return (cardButtons.count + 1) / 2
+            return (visibleCardButtons.count + 1) / 2
         }
     }
 //    private var emojiChoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
@@ -37,36 +37,20 @@ class ViewController: UIViewController {
     // MARK: IBActions
     @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
-        if let cardNumber = cardButtons.firstIndex(of: sender) {
-            game.chooseCard(at: cardNumber)
-            updateViewFromModel()
-        } else {
-            
-        }
     }
     
     // MARK: Methods
     private func updateViewFromModel() {
-        for index in cardButtons.indices {
-            let button = cardButtons[index]
-            let card = game.cards[index]
-            
-            if card.isFaceUp {
-                button.setTitle(emoji(for: card), for: .normal)
-                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-            } else {
-                button.setTitle("", for: .normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-            }
-        }
     }
     
     private func updateFlipCountLabel() {
         let attributes: [NSAttributedString.Key : Any] = [
             .strokeWidth : 5.0,
-            .strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            .strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         ]
-        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        let attributedString = NSAttributedString(string:
+            traitCollection.verticalSizeClass == .compact ? "Flips: \n \(flipCount)" : "Flips: \(flipCount)",
+            attributes: attributes)
         flipCountLabel.attributedText = attributedString
     }
     
@@ -86,8 +70,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-}
-
 extension Int {
     var arc4random: Int {
         if self > 0 {

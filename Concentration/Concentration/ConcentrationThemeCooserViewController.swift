64bc24bcb70b7536ba8dcefe1beb065f9d09d801
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-08-18 20:57:28 +0100

    Added ConcentrationThemeChooserViewController
    Added ConcentrationThemeChooserViewController class for choosing game theme

---
+++ Concentration/Concentration/ConcentrationThemeCooserViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ConcentrationThemeCooserViewController.swift
+//  Concentration
+//
+//  Created by Mukhtar Yusuf on 7/9/20.
+//  Copyright © 2020 Mukhtar Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationThemeCooserViewController: UIViewController, UISplitViewControllerDelegate {
+}

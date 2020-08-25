12b7776128912d80dde7be61f15261d1aab49e74
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-08-25 21:35:44 +0100

    Added prepare() method
    Added prepare(for:, sender:) method to prepare for segueing to ConcentrationViewController after picking theme

--- Concentration/Concentration/ConcentrationThemeCooserViewController.swift
+++ Concentration/Concentration/ConcentrationThemeCooserViewController.swift
@@ -56,4 +56,15 @@ class ConcentrationThemeCooserViewController: UIViewController, UISplitViewContr
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Choose Theme" {
+            if let themeName = (sender as? UIButton)?.currentTitle,
+                let theme = themes[themeName] {
+                if let cvc = segue.destination as? ConcentrationViewController {
+                    cvc.theme = theme
+                    lastSeguedToConcentrationViewController = cvc
+                }
+            }
+        }
+    }
 }
